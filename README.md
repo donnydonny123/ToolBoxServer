a7b05d31418ef1c514c980d4ffec3af54c018e9e
donnydonny123 <don.cricketsss@gmail.com>
2018-01-30 00:29:10 +0800

    auth and index downloading

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-###Donny's toolbox server
+### Donny's toolbox server
 
 ## little practice of [kotlin](https://github.com/donnydonny123/ToolBox) and flask server
 
-# Day1 & 2
+# Day 3 
+- auth function <http://flask.pocoo.org/snippets/8/>
+- send file through index not filename
+- add TODO
+
+# Day 1 & 2
 - flask hello world server
 - add file downloading through api call 
 ***
 > 可以從browser裡看到file了 可是APP端能不能用還不知道
 >
-> 感覺很危險的server 只要開了就可以access整台電腦(連檔案都印給你看)
->
 > 看要[加個auth](https://github.com/lpsandaruwan/pynix-file-server/blob/master/src/controller/api_handler.py)或怎樣的
+
+***
+Future Expansion:
+- https
+- youtube playlist fetch
+- youtube download
+- android widget
+- crawl fetch
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory, Response, jsonify
+from flask import Flask, send_from_directory, Response, jsonify, request
+from functools import wraps
 import os
 import config
 app = Flask(__name__)
@@ -6,15 +7,41 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+# auth from http://flask.pocoo.org/snippets/8/
+def check_auth(username, password):
+    """This function is called to check if a username / password combination is valid."""
+    return username == 'admin' and password == 'secret'
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
+
 @app.route("/files")
+@requires_auth
 def get_file_list():
     return jsonify(os.listdir(config.file_directory))
 
-@app.route("/files/<filename>", methods=["GET"])
-def get_file_by_name(filename):
-    print(filename, os.listdir(config.file_directory))
-    if filename in os.listdir(config.file_directory):
-        return send_from_directory(directory=os.path.abspath(config.file_directory), filename=filename)
+@app.route("/files/<int:fileIndex>", methods=["GET"])
+@requires_auth
+def get_file_by_name(fileIndex):
+    listOfFiles = os.listdir(config.file_directory)
+    print(fileIndex, listOfFiles)
+    if fileIndex < len(os.listdir(config.file_directory)):
+        return send_from_directory(directory=os.path.abspath(config.file_directory), filename=listOfFiles[fileIndex])
     else:
         return Response(
             filename + " is not found in the server\n",
